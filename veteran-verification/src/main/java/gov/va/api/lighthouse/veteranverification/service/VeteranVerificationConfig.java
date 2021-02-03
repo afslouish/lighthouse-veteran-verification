236b4efc6723dfd187fb3b95f35c890a21839be8
Luke Markwordt <luke.markwordt@libertyits.com>
2021-02-02 17:11:46 -0700

    Created a configuration class for veteran-verification

---
+++ veteran-verification/src/main/java/gov/va/api/lighthouse/veteranverification/service/VeteranVerificationConfig.java
@@ -0,0 +1,30 @@
+package gov.va.api.lighthouse.veteranverification.service;
+
+import gov.va.api.lighthouse.emis.EmisConfigV1;
+import gov.va.api.lighthouse.emis.EmisVeteranStatusServiceClient;
+import gov.va.api.lighthouse.emis.SoapEmisVeteranStatusServiceClient;
+import gov.va.api.lighthouse.mpi.MasterPatientIndexClient;
+import gov.va.api.lighthouse.mpi.MpiConfig;
+import gov.va.api.lighthouse.mpi.SoapMasterPatientIndexClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VeteranVerificationConfig {
+    MpiConfig mpiConfig;
+    EmisConfigV1 emisConfigV1;
+    VeteranVerificationConfig(@Autowired MpiConfig mpiConfig, @Autowired EmisConfigV1 emisConfigV1) {
+        this.mpiConfig = mpiConfig;
+        this.emisConfigV1 = emisConfigV1;
+    }
+    @Bean
+    public MasterPatientIndexClient masterPatientIndexClient() {
+        return SoapMasterPatientIndexClient.of(mpiConfig);
+    }
+
+    @Bean
+    public EmisVeteranStatusServiceClient emisVeteranStatusServiceClient() {
+        return SoapEmisVeteranStatusServiceClient.of(emisConfigV1);
+    }
+}
