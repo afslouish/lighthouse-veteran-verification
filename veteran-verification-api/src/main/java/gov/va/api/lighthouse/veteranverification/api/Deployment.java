340299d4f650761e2d7df330603c78db52f9477c
lukemarkwordtlibertyits <luke.markwordt@libertyits.com>
2021-06-29 14:51:59 -0600

    Fully implemented deployment schema

--- veteran-verification-api/src/main/java/gov/va/api/lighthouse/veteranverification/api/Deployment.java
+++ veteran-verification-api/src/main/java/gov/va/api/lighthouse/veteranverification/api/Deployment.java
@@ -1,17 +1,41 @@
 package gov.va.api.lighthouse.veteranverification.api;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Date;
+import lombok.Builder;
+import lombok.Value;
+import lombok.experimental.Accessors;
 
+@Value
+@Builder
+@Accessors(fluent = false)
 @Schema(type = "Object", description = "military episode deployment")
+@SuppressFBWarnings({"EI_EXPOSE_REP2", "EI_EXPOSE_REP"})
 public class Deployment {
-  @Schema(name = "start_date", description = "deployment start date", example = "2000-01-20")
+
+  @Schema(
+      name = "start_date",
+      description = "deployment start date",
+      example = "2000-01-20",
+      required = true,
+      nullable = true)
   Date startDate;
 
-  @Schema(name = "end_date", description = "deployment end date", example = "2001-01-20")
+  @Schema(
+      name = "end_date",
+      description = "deployment end date",
+      example = "2001-01-20",
+      required = true,
+      nullable = true)
   Date endDate;
 
-  @Schema(name = "location", description = "deployment location", example = "AFG")
+  @Schema(
+      name = "location",
+      description = "deployment location",
+      example = "AFG",
+      required = true,
+      nullable = true)
   Location location;
 
   enum Location {}
