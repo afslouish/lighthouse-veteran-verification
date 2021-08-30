963c90ed70ebda46860b562f00870d78ca171cdd
lukemarkwordtlibertyits <luke.markwordt@libertyits.com>
2021-08-30 13:24:25 -0600

    Added JwksProperties to Notary Pattern

--- veteran-verification/src/main/java/gov/va/api/lighthouse/veteranverification/service/utils/Notary.java
+++ veteran-verification/src/main/java/gov/va/api/lighthouse/veteranverification/service/utils/Notary.java
@@ -8,50 +8,16 @@ import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSObject;
 import com.nimbusds.jose.crypto.RSASSASigner;
-import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jwt.JWTClaimsSet;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.security.MessageDigest;
 import java.util.Map;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.SneakyThrows;
-import org.bouncycastle.asn1.ASN1EncodableVector;
-import org.bouncycastle.asn1.ASN1Integer;
-import org.bouncycastle.asn1.ASN1OutputStream;
-import org.bouncycastle.asn1.ASN1Sequence;
-import org.bouncycastle.asn1.DERSequence;
-import org.bouncycastle.util.encoders.Hex;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@AllArgsConstructor(onConstructor_ = @Autowired)
 public class Notary {
-  private final JWK privateKey;
-
-  /**
-   * Builder for Notary.
-   *
-   * @param privateKey File that contains the private key.
-   */
-  @SneakyThrows
-  public Notary(@NonNull File privateKey) {
-    this.privateKey = JWK.parseFromPEMEncodedObjects(Files.readString(privateKey.toPath()));
-  }
-
-  @SneakyThrows
-  private String kid() {
-    ASN1EncodableVector vector = new ASN1EncodableVector();
-    vector.add(new ASN1Integer(privateKey.toRSAKey().getModulus().decodeToBigInteger()));
-    vector.add(new ASN1Integer(privateKey.toRSAKey().getPublicExponent().decodeToBigInteger()));
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    ASN1OutputStream asnOs = new ASN1OutputStream(baos);
-    asnOs.writeObject(new DERSequence(vector));
-    asnOs.flush();
-    ASN1Sequence sequence = (ASN1Sequence) ASN1Sequence.fromByteArray(baos.toByteArray());
-    MessageDigest digest = MessageDigest.getInstance("SHA-256");
-    byte[] hash = digest.digest(sequence.getEncoded());
-    return new String(Hex.encode(hash), StandardCharsets.UTF_8);
-  }
+  @NonNull private final JwksProperties jwksProperties;
 
   @SuppressWarnings("unchecked")
   private JWTClaimsSet objectToClaimSet(Object object) {
@@ -73,14 +39,16 @@ public class Notary {
    * @param object object to be converted into a jwt.
    * @return Jwt representation of the map.
    */
-  @SuppressWarnings("deprecation")
   @SneakyThrows
   public String objectToJwt(@NonNull Object object) {
     JWSObject jwsObject =
         new JWSObject(
-            new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(kid()).type(JOSEObjectType.JWT).build(),
+            new JWSHeader.Builder(JWSAlgorithm.RS256)
+                .keyID(jwksProperties.currentKeyId())
+                .type(JOSEObjectType.JWT)
+                .build(),
             objectToClaimSet(object).toPayload());
-    jwsObject.sign(new RSASSASigner(privateKey.toRSAKey(), true));
+    jwsObject.sign(new RSASSASigner(jwksProperties.currentPrivateJwk().toRSAKey()));
     return jwsObject.serialize();
   }
 }
--- veteran-verification/src/test/java/gov/va/api/lighthouse/veteranverification/service/utils/NotaryTest.java
+++ veteran-verification/src/test/java/gov/va/api/lighthouse/veteranverification/service/utils/NotaryTest.java
@@ -2,7 +2,8 @@ package gov.va.api.lighthouse.veteranverification.service.utils;
 
 import gov.va.api.lighthouse.veteranverification.api.v0.ServiceHistoryResponse;
 import gov.va.api.lighthouse.veteranverification.service.TestUtils;
-import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyStore;
 import java.util.Arrays;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
@@ -10,22 +11,11 @@ import org.junit.jupiter.api.Test;
 
 public class NotaryTest {
   @Test
-  public void constructorFileIsNonNull() {
+  public void jwksPropertiesIsNonNull() {
     Assertions.assertThrows(
         NullPointerException.class,
         () -> {
           new Notary(null);
-        });
-  }
-
-  @Test
-  @SneakyThrows
-  public void objectToJwtObjectIsNonNull() {
-    Notary notary = new Notary(new File("src/test/resources/verification_test_private.pem"));
-    Assertions.assertThrows(
-        NullPointerException.class,
-        () -> {
-          notary.objectToJwt(null);
         });
   }
 
@@ -39,21 +29,39 @@ public class NotaryTest {
         ServiceHistoryResponse.builder()
             .data(Arrays.stream(serviceHistoryEpisodes).toList())
             .build();
-    Notary notary = new Notary(new File("src/test/resources/verification_test_private.pem"));
+    KeyStore ks = KeyStore.getInstance("jks");
+    ks.load(new FileInputStream("src/test/resources/fakekeystore.jks"), "secret".toCharArray());
+    JwksProperties jwksProperties = new JwksProperties(ks, "fake", "secret");
+    Notary notary = new Notary(jwksProperties);
     String jwt = notary.objectToJwt(serviceHistoryResponse);
     Assertions.assertEquals(
-        "eyJraWQiOiIwODhkMjQyMzJmZjZmYWE0Y2Q0Y2ZlYzEyNmFkMDQzMWRmZjFlYTAyOGFmZGIxYzg2YjM3MThkNzAxN"
-            + "zFhZWQ2IiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ.eyJkYXRhIjpbeyJpZCI6Im1vY2siLCJ0eXBlIjoic2Vydm"
-            + "ljZS1oaXN0b3J5LWVwaXNvZGVzIiwiYXR0cmlidXRlcyI6eyJmaXJzdF9uYW1lIjoiSm9obiIsImxhc3RfbmFtZSI6Ik"
-            + "RvZSIsImJyYW5jaF9vZl9zZXJ2aWNlIjoiQnJhbmNoT2ZTZXJ2aWNlIiwic3RhcnRfZGF0ZSI6IjIwMDAtMDEtMDEiLC"
-            + "JlbmRfZGF0ZSI6IjIwMDEtMDEtMDEiLCJwYXlfZ3JhZGUiOiJQYXlHcmFkZSIsImRpc2NoYXJnZV9zdGF0dXMiOiJob2"
-            + "5vcmFibGUiLCJzZXBhcmF0aW9uX3JlYXNvbiI6IlNlcGFyYXRpb25SZWFzb24iLCJkZXBsb3ltZW50cyI6W3sic3Rhcn"
-            + "RfZGF0ZSI6IjIwMDAtMDItMDEiLCJlbmRfZGF0ZSI6IjIwMDEtMDEtMDEiLCJsb2NhdGlvbiI6IkFGRyJ9LHsic3Rhcn"
-            + "RfZGF0ZSI6IjIwMDItMDItMDEiLCJlbmRfZGF0ZSI6IjIwMDMtMDEtMDEiLCJsb2NhdGlvbiI6IkFGRyJ9LHsic3Rhcn"
-            + "RfZGF0ZSI6IjIwMDQtMDItMDEiLCJlbmRfZGF0ZSI6IjIwMDUtMDEtMDEiLCJsb2NhdGlvbiI6IkFGRyJ9LHsic3Rhcn"
-            + "RfZGF0ZSI6IjIwMDYtMDItMDEiLCJlbmRfZGF0ZSI6IjIwMDctMDEtMDEiLCJsb2NhdGlvbiI6IkFGRyJ9XX19XX0.uM"
-            + "bTDYFXSfdJovBJMOdlPwF9USHGOd6ksG-ZJrBnOa8vwFBQ-h6lDV6qg73W_gt64DwXj1wT4o-NUiS5rfs9czLwx-Ck9V"
-            + "PHmatQZVyrnWkHkWPLlWve11kIs1nHOWEG1m28zug4s5SyafGfC6whmXb6VKlWtY_Ll6Gy0dMQqns",
+        "eyJraWQiOiJmYWtlIiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ.eyJkYXRhIjpbeyJpZCI6Im1vY2siLCJ0eX"
+            + "BlIjoic2VydmljZS1oaXN0b3J5LWVwaXNvZGVzIiwiYXR0cmlidXRlcyI6eyJmaXJzdF9uYW1lIjoiSm9obiIsImxhc3R"
+            + "fbmFtZSI6IkRvZSIsImJyYW5jaF9vZl9zZXJ2aWNlIjoiQnJhbmNoT2ZTZXJ2aWNlIiwic3RhcnRfZGF0ZSI6IjIwMDAt"
+            + "MDEtMDEiLCJlbmRfZGF0ZSI6IjIwMDEtMDEtMDEiLCJwYXlfZ3JhZGUiOiJQYXlHcmFkZSIsImRpc2NoYXJnZV9zdGF0d"
+            + "XMiOiJob25vcmFibGUiLCJzZXBhcmF0aW9uX3JlYXNvbiI6IlNlcGFyYXRpb25SZWFzb24iLCJkZXBsb3ltZW50cyI6W3"
+            + "sic3RhcnRfZGF0ZSI6IjIwMDAtMDItMDEiLCJlbmRfZGF0ZSI6IjIwMDEtMDEtMDEiLCJsb2NhdGlvbiI6IkFGRyJ9LHs"
+            + "ic3RhcnRfZGF0ZSI6IjIwMDItMDItMDEiLCJlbmRfZGF0ZSI6IjIwMDMtMDEtMDEiLCJsb2NhdGlvbiI6IkFGRyJ9LHsi"
+            + "c3RhcnRfZGF0ZSI6IjIwMDQtMDItMDEiLCJlbmRfZGF0ZSI6IjIwMDUtMDEtMDEiLCJsb2NhdGlvbiI6IkFGRyJ9LHsic"
+            + "3RhcnRfZGF0ZSI6IjIwMDYtMDItMDEiLCJlbmRfZGF0ZSI6IjIwMDctMDEtMDEiLCJsb2NhdGlvbiI6IkFGRyJ9XX19XX"
+            + "0.dmBxYrcELzUq0ntBHY2ZD2PvrCV6rNJBTuvXUr3VxXirzoY6vB4JPmTAznbviRjvS44D4z9uJat0UUhko9PoMRIfYCr"
+            + "MqY-9crbGVmu5P1RJoiHLxL_HAykygov3wMKC_yzeIS_IJ2UIw_NIzuaLZDuk9HE6WJCVcs0A_Yjiv6thRgVjUD-08gbT"
+            + "Q0Oz38UH3xdDSyNozUXH5AHFpkkC8DWl4MDg774oXiMBCAzjfw7uR4FFHXXAqZJZR7uPKVbASxCcmqHKH5JInnZjQ_1Fe"
+            + "QSxKF0FXQx-_XzjoIvKshS43_f3277ok_eT9zZlStzNrtLgZmuSLb3eSe8U6UFLkg",
         jwt);
+  }
+
+  @Test
+  @SneakyThrows
+  public void objectToJwtObjectIsNonNull() {
+    KeyStore ks = KeyStore.getInstance("jks");
+    ks.load(new FileInputStream("src/test/resources/fakekeystore.jks"), "secret".toCharArray());
+    JwksProperties jwksProperties = new JwksProperties(ks, "fake", "fake");
+    Notary notary = new Notary(jwksProperties);
+    Assertions.assertThrows(
+        NullPointerException.class,
+        () -> {
+          notary.objectToJwt(null);
+        });
   }
 }
