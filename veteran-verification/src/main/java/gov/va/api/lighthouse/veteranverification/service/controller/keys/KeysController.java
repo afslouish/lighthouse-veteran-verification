61260d8a28f166b77ea9194550ec1b407daaa1f4
lukemarkwordtlibertyits <luke.markwordt@libertyits.com>
2021-09-02 10:48:13 -0600

    made keysController a rest endpoint

--- veteran-verification/src/main/java/gov/va/api/lighthouse/veteranverification/service/controller/keys/KeysController.java
+++ veteran-verification/src/main/java/gov/va/api/lighthouse/veteranverification/service/controller/keys/KeysController.java
@@ -3,7 +3,11 @@ package gov.va.api.lighthouse.veteranverification.service.controller.keys;
 import gov.va.api.lighthouse.veteranverification.api.v0.JwkKeyset;
 import gov.va.api.lighthouse.veteranverification.service.utils.JwksProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(produces = {"application/json"})
 public class KeysController {
   JwksProperties jwksProperties;
 
