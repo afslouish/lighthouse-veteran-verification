aded7f2195de32a933b91276fd2b4b6b441c92ad
lukemarkwordtlibertyits <luke.markwordt@libertyits.com>
2021-09-10 14:13:51 -0600

    refactored the error handler to always return JSON

--- veteran-verification/src/main/java/gov/va/api/lighthouse/veteranverification/service/WebExceptionHandler.java
+++ veteran-verification/src/main/java/gov/va/api/lighthouse/veteranverification/service/WebExceptionHandler.java
@@ -7,40 +7,42 @@ import gov.va.api.lighthouse.veteranverification.api.ApiError.EmisInaccessibleWs
 import gov.va.api.lighthouse.veteranverification.api.ApiError.InaccessibleWsdlErrorApiError;
 import gov.va.api.lighthouse.veteranverification.api.ApiError.NoServiceHistoryFoundApiError;
 import gov.va.api.lighthouse.veteranverification.service.Exceptions.EmisInaccesibleWsdlException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /** Handler for all web exceptions that returns a meaningful error response object. */
 @RestControllerAdvice
-@RequestMapping(produces = "application/json")
 public class WebExceptionHandler {
   /** Return error for EMIS inaccessible wsdl exception. */
   @ExceptionHandler({EmisInaccesibleWsdlException.class})
-  @ResponseStatus(HttpStatus.BAD_GATEWAY)
-  public EmisInaccessibleWsdlErrorApiError handleEmisInaccessibleWsdlException() {
-    return new EmisInaccessibleWsdlErrorApiError();
+  public ResponseEntity handleEmisInaccessibleWsdlException() {
+    return new ResponseEntity(new EmisInaccessibleWsdlErrorApiError(), createErrorHeaders(),HttpStatus.BAD_GATEWAY);
   }
 
   /** Return error for inaccessible wsdl exception. */
   @ExceptionHandler({InaccessibleWSDLException.class})
-  @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
-  public InaccessibleWsdlErrorApiError handleInaccessibleWsdlException() {
-    return new InaccessibleWsdlErrorApiError();
+  public ResponseEntity handleInaccessibleWsdlException() {
+    return new ResponseEntity(new InaccessibleWsdlErrorApiError(), createErrorHeaders(),HttpStatus.BAD_GATEWAY);
   }
 
   /** Return error for EMIS inaccessible wsdl exception. */
   @ExceptionHandler({Exceptions.NoServiceHistoryFoundException.class})
-  @ResponseStatus(HttpStatus.BAD_GATEWAY)
-  public NoServiceHistoryFoundApiError handleNoServiceHistoryFoundException() {
-    return new NoServiceHistoryFoundApiError();
+  public ResponseEntity handleNoServiceHistoryFoundException() {
+    return new ResponseEntity(new NoServiceHistoryFoundApiError(), createErrorHeaders(),HttpStatus.BAD_GATEWAY);
   }
 
   @ExceptionHandler({ServerSOAPFaultException.class})
-  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-  public ApiError handleSoapFaultException(ServerSOAPFaultException e) {
-    return new ApiError.ServerSoapFaultApiError();
+  public ResponseEntity handleSoapFaultException(ServerSOAPFaultException e) {
+    return new ResponseEntity(new ApiError.ServerSoapFaultApiError(), createErrorHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  private HttpHeaders createErrorHeaders() {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    return headers;
   }
 }
