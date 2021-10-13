40367d20ed492884e4c8a00d737792a1163f01cf
lukemarkwordtlibertyits <luke.markwordt@libertyits.com>
2021-10-13 16:26:43 -0600

    BackendHealthController only checks for Rest exception

--- veteran-verification/src/main/java/gov/va/api/lighthouse/veteranverification/service/controller/health/BackendHealthController.java
+++ veteran-verification/src/main/java/gov/va/api/lighthouse/veteranverification/service/controller/health/BackendHealthController.java
@@ -1,7 +1,5 @@
 package gov.va.api.lighthouse.veteranverification.service.controller.health;
 
-import com.sun.xml.ws.wsdl.parser.InaccessibleWSDLException;
-import java.net.MalformedURLException;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -81,9 +79,7 @@ public class BackendHealthController {
     try {
       ResponseEntity<String> response = healthCheck.call();
       status = response.getStatusCode();
-    } catch (RestClientException | InaccessibleWSDLException | MalformedURLException e) {
-      // InaccessibleWSDLException and MalformedURLException are thrown by Lighthouse SOAP clients
-      // RestClientException is thrown by Lighthouse REST clients
+    } catch (RestClientException e) {
       log.error("Failure occurred when getting {} health: {}", name, e.getMessage());
       status = HttpStatus.INTERNAL_SERVER_ERROR;
     }
--- veteran-verification/src/test/java/gov/va/api/lighthouse/veteranverification/service/controller/health/BackendHealthControllerTest.java
+++ veteran-verification/src/test/java/gov/va/api/lighthouse/veteranverification/service/controller/health/BackendHealthControllerTest.java
@@ -6,8 +6,6 @@ import static org.mockito.Mockito.when;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
-import com.sun.xml.ws.wsdl.parser.InaccessibleWSDLException;
-import java.net.MalformedURLException;
 import java.util.AbstractMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -32,10 +30,6 @@ public class BackendHealthControllerTest {
 
   @Mock Callable<ResponseEntity<String>> restClientExceptionResponse;
 
-  @Mock Callable<ResponseEntity<String>> inaccessibleWSDLExceptionResponse;
-
-  @Mock Callable<ResponseEntity<String>> malformedURLExceptionResponse;
-
   private ListAppender<ILoggingEvent> listAppender;
 
   @BeforeEach
@@ -49,12 +43,6 @@ public class BackendHealthControllerTest {
 
     restClientExceptionResponse = mock(Callable.class);
     when(restClientExceptionResponse.call()).thenThrow(mock(RestClientException.class));
-
-    inaccessibleWSDLExceptionResponse = mock(Callable.class);
-    when(inaccessibleWSDLExceptionResponse.call()).thenThrow(mock(InaccessibleWSDLException.class));
-
-    malformedURLExceptionResponse = mock(Callable.class);
-    when(malformedURLExceptionResponse.call()).thenThrow(mock(MalformedURLException.class));
   }
 
   @Test
@@ -91,64 +79,6 @@ public class BackendHealthControllerTest {
     Assertions.assertEquals(200, dependencies.get(0).getDetails().get("httpCode"));
     Assertions.assertEquals(HttpStatus.OK, dependencies.get(0).getDetails().get("status"));
     Assertions.assertNotNull(dependencies.get(0).getDetails().get("time"));
-  }
-
-  @Test
-  public void collectBackendHealthOneBackendServiceReturnInaccessibleWSDLExceptionResponse() {
-    Map<String, Callable<ResponseEntity<String>>> map = new LinkedHashMap<>();
-    map.put(upService.getKey(), upService.getValue());
-    downService = new AbstractMap.SimpleEntry<>("Down Service", inaccessibleWSDLExceptionResponse);
-    map.put(downService.getKey(), downService.getValue());
-    ResponseEntity<Health> health = new BackendHealthController(map).collectBackendHealth();
-    Assertions.assertEquals(HttpStatus.SERVICE_UNAVAILABLE, health.getStatusCode());
-    Assertions.assertEquals(
-        "Veteran-Verification Backend Health Check", health.getBody().getStatus().getDescription());
-    Assertions.assertEquals("DOWN", health.getBody().getStatus().getCode());
-    Assertions.assertNotNull(health.getBody().getDetails().get("time"));
-    List<Health> dependencies = (List<Health>) health.getBody().getDetails().get("backendServices");
-    Assertions.assertEquals(2, dependencies.size());
-    Assertions.assertEquals("Up Service", dependencies.get(0).getStatus().getDescription());
-    Assertions.assertEquals("UP", dependencies.get(0).getStatus().getCode());
-    Assertions.assertEquals("Up Service", dependencies.get(0).getDetails().get("name"));
-    Assertions.assertEquals(200, dependencies.get(0).getDetails().get("httpCode"));
-    Assertions.assertEquals(HttpStatus.OK, dependencies.get(0).getDetails().get("status"));
-    Assertions.assertNotNull(dependencies.get(0).getDetails().get("time"));
-    Assertions.assertEquals("Down Service", dependencies.get(1).getStatus().getDescription());
-    Assertions.assertEquals("DOWN", dependencies.get(1).getStatus().getCode());
-    Assertions.assertEquals("Down Service", dependencies.get(1).getDetails().get("name"));
-    Assertions.assertEquals(500, dependencies.get(1).getDetails().get("httpCode"));
-    Assertions.assertEquals(
-        HttpStatus.INTERNAL_SERVER_ERROR, dependencies.get(1).getDetails().get("status"));
-    Assertions.assertNotNull(dependencies.get(1).getDetails().get("time"));
-  }
-
-  @Test
-  public void collectBackendHealthOneBackendServiceReturnMalformedURLExceptionResponse() {
-    Map<String, Callable<ResponseEntity<String>>> map = new LinkedHashMap<>();
-    map.put(upService.getKey(), upService.getValue());
-    downService = new AbstractMap.SimpleEntry<>("Down Service", malformedURLExceptionResponse);
-    map.put(downService.getKey(), downService.getValue());
-    ResponseEntity<Health> health = new BackendHealthController(map).collectBackendHealth();
-    Assertions.assertEquals(HttpStatus.SERVICE_UNAVAILABLE, health.getStatusCode());
-    Assertions.assertEquals(
-        "Veteran-Verification Backend Health Check", health.getBody().getStatus().getDescription());
-    Assertions.assertEquals("DOWN", health.getBody().getStatus().getCode());
-    Assertions.assertNotNull(health.getBody().getDetails().get("time"));
-    List<Health> dependencies = (List<Health>) health.getBody().getDetails().get("backendServices");
-    Assertions.assertEquals(2, dependencies.size());
-    Assertions.assertEquals("Up Service", dependencies.get(0).getStatus().getDescription());
-    Assertions.assertEquals("UP", dependencies.get(0).getStatus().getCode());
-    Assertions.assertEquals("Up Service", dependencies.get(0).getDetails().get("name"));
-    Assertions.assertEquals(200, dependencies.get(0).getDetails().get("httpCode"));
-    Assertions.assertEquals(HttpStatus.OK, dependencies.get(0).getDetails().get("status"));
-    Assertions.assertNotNull(dependencies.get(0).getDetails().get("time"));
-    Assertions.assertEquals("Down Service", dependencies.get(1).getStatus().getDescription());
-    Assertions.assertEquals("DOWN", dependencies.get(1).getStatus().getCode());
-    Assertions.assertEquals("Down Service", dependencies.get(1).getDetails().get("name"));
-    Assertions.assertEquals(500, dependencies.get(1).getDetails().get("httpCode"));
-    Assertions.assertEquals(
-        HttpStatus.INTERNAL_SERVER_ERROR, dependencies.get(1).getDetails().get("status"));
-    Assertions.assertNotNull(dependencies.get(1).getDetails().get("time"));
   }
 
   @Test
